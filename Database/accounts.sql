130cf2f5ad170177a311ba465751861e245bda8c
Vuyani Magibisela <vuyani.magibisela@live.com>
2024-05-20 21:59:01 +0200

    update database

--- Database/accounts.sql
+++ Database/accounts.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2023 at 06:46 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: May 20, 2024 at 09:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `accounts`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `checked_in` datetime DEFAULT NULL,
+  `checked_out` datetime DEFAULT NULL,
+  `sign_in_status` enum('signedIn','signedOut') DEFAULT 'signedOut'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `user_id`, `checked_in`, `checked_out`, `sign_in_status`) VALUES
+(8, 1, '2024-05-13 19:01:56', '2024-05-13 20:51:33', 'signedOut'),
+(9, 8, '2024-05-13 19:03:20', '2024-05-13 20:41:44', 'signedOut'),
+(10, 7, '2024-05-13 20:11:19', '2024-05-13 20:41:45', 'signedOut'),
+(11, 9, '2024-05-13 20:42:36', '2024-05-13 20:42:41', 'signedOut'),
+(12, 9, '2024-05-14 06:57:03', '2024-05-14 14:25:27', 'signedOut'),
+(13, 9, '2024-05-14 12:54:12', '2024-05-14 14:25:27', 'signedOut'),
+(14, 1, '2024-05-14 12:55:16', '2024-05-14 15:04:33', 'signedOut'),
+(15, 8, '2024-05-14 12:55:37', '2024-05-14 12:56:19', 'signedOut'),
+(16, 7, '2024-05-14 12:56:02', '2024-05-14 12:56:22', 'signedOut'),
+(17, 9, '2024-05-14 14:25:22', '2024-05-14 14:25:27', 'signedOut'),
+(18, 1, '2024-05-14 15:04:29', '2024-05-14 15:04:33', 'signedOut'),
+(19, 9, '2024-05-15 11:21:15', '2024-05-15 11:22:31', 'signedOut');
 
 -- --------------------------------------------------------
 
@@ -44,7 +75,7 @@ CREATE TABLE `users` (
   `leaner number` int(10) NOT NULL,
   `parent number` int(10) NOT NULL,
   `Relationship` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
@@ -52,13 +83,24 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `name`, `surname`, `user_type`, `date of birth`, `grade`, `school`, `parent`, `parent email`, `leaner number`, `parent number`, `Relationship`) VALUES
 (1, 'vuyani_magibisela', '', '$2y$10$OEkQUqNT9pp8F.oBn/nAquaBuxyo7.8a0QCiWLXw37ECwvzXWNZDy', 'Vuyani', 'Magibisela', 'admin', '0000-00-00', 0, '', '', 0, 0, 0, ''),
-(2, 'itumeleng_kgakane', '', '$2y$10$d.dK2Y1gx9G7kUrWTE.y3OgNA19ciUScf7tRl7QXBnV7Yndp8cYE.', 'Itumeleng', 'Kgakane', 'member', '0000-00-00', 0, '', '', 0, 0, 0, ''),
-(4, 'themba_magibisela', '', '$2y$10$gN04DqX.nz2Epf.oiGBlsev18477uJ6srSHX7yjEEpB3fuqVT7M5G', 'Themba', 'Magibisela', 'mentor', '0000-00-00', 0, '', '', 0, 0, 0, ''),
-(5, 'themba_kgakane', '', '$2y$10$MF.CYKDPS86B4KofOayZWuSDP7ETZtLpiVXnyDO2aU3bnn4K8BHzW', 'Themba', 'Kgakane', 'mentor', '0000-00-00', 0, '', '', 0, 0, 0, '');
+(2, 'itumeleng_kgakane', '', '54321', 'Itumeleng', 'Kgakane', 'member', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(4, 'themba_magibisela', '', '13378', 'Themba', 'Magibisela', 'mentor', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(5, 'themba_kgakane', '', '$2y$10$MF.CYKDPS86B4KofOayZWuSDP7ETZtLpiVXnyDO2aU3bnn4K8BHzW', 'Themba', 'Kgakane', 'mentor', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(6, 'Kgotso_Maponya', '', '$2y$10$8BzYI1MeWyctdaCnC6oJzufCqMd2CFSzhseZYBPz.0pQtofyv1mya', 'Kgotso', 'Maponya', 'member', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(7, 'Sam_King', '', '$2y$10$aSK4SUKKNstMnefJ0VKfs.NyBF32SHBC5wv.ALf2w1HvBrK1hLqgK', 'Sam', 'Kabanga', 'member', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(8, 'jabu_khumalo ', '', '$2y$10$n8Pb/bmu/7rO7KJSlXkaFeaTjabFz7anFGZmxtvWmkvADhZVbIh0S', 'Jabu', 'Khumalo', 'mentor', '0000-00-00', 0, '', '', 0, 0, 0, ''),
+(9, 'lebo_skhosana', '', '$2y$10$4.fc0AJKIOfj.YPKp0sePOtDllx98DA8yIMl14wv5hptlsDMEY84O', 'Lebo', 'Skhosana', 'admin', '0000-00-00', 0, '', '', 0, 0, 0, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -71,10 +113,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
