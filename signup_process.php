b7f5df83ac5ced5172e7bdbcdd57caca8c6ae8d1
Vuyani Magibisela <vuyani.magibisela@live.com>
2024-07-02 06:12:45 +0200

    updated singup processing to show errors if not working

--- signup_process.php
+++ signup_process.php
@@ -1,9 +1,14 @@
 <?php
-	session_start();
-?>
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-<?php
 require 'server.php';
+
+// Check connection
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
 
 // If the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -21,21 +26,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "INSERT INTO users (name, surname, username, user_type, password) VALUES (?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
 
+    if (!$stmt) {
+        die("Error in prepare statement: " . mysqli_error($conn));
+    }
+
     // Bind the parameters to the prepared statement
-    mysqli_stmt_bind_param($stmt, 'sssss', $name, $surname, $username, $userType, $hashed_password);
+    if (!mysqli_stmt_bind_param($stmt, 'sssss', $name, $surname, $username, $userType, $hashed_password)) {
+        die("Error binding parameters: " . mysqli_stmt_error($stmt));
+    }
 
     // Execute the prepared statement
-    if (mysqli_stmt_execute($stmt)) {
+    if (!mysqli_stmt_execute($stmt)) {
+        die("Error executing statement: " . mysqli_stmt_error($stmt));
+    }
+
+    // Check if any rows were affected
+    $affected_rows = mysqli_stmt_affected_rows($stmt);
+    if ($affected_rows == 1) {
         // New user created successfully, start session
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
         header("Location: home.php");
         exit();
     } else {
-        // Error creating new user, redirect to sign up page with error message
-        $_SESSION['signup_error'] = "Error creating new user";
-        header("Location: signup.php");
-        exit();
+        // No rows affected, which is unexpected
+        die("Insert statement did not affect any rows. Affected rows: " . $affected_rows);
     }
 
     // Close the statement
