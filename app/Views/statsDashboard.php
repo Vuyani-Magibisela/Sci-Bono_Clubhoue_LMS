780cbdcf5e50c580b452446670a1f30f194dfad1
Vuyani Magibisela <vuyani.magibisela@live.com>
2025-01-30 05:36:20 +0200

    added error logging on statsDashboard

--- app/Views/statsDashboard.php
+++ app/Views/statsDashboard.php
@@ -1,5 +1,11 @@
 <?php
-include '../Models/dashboardStats.php';
+include __DIR__ . '/../Models/dashboardStats.php';
+
+//error logging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
 
 <!DOCTYPE html>
