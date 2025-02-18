f2eb106540f672670bd967b8a99e443f17183947
Vuyani  Magibisela <vuyanim@sci-bono.co.za>
2025-02-18 17:20:25 +0200

    updated login style

--- login.php
+++ login.php
@@ -44,10 +44,10 @@
 			<div>
 			<h2>Log In</h2>
 			<form name="loginForm" action="login_process.php" method="post" onsubmit="return validateForm()">
-				<label for="username">Username:</label><br><br>
-				<input class="input_field" type="text" id="username" name="username"><br><br>
-				<label for="password">Password:</label><br><br>
-				<input class="input_field" type="password" id="password" name="password"><br><br>
+				<label for="username">Username:</label>
+				<input class="input_field" type="text" id="username" name="username"><br>
+				<label for="password">Password:</label>
+				<input class="input_field" type="password" id="password" name="password"><br>
 				<input class="btn" type="submit" value="Login"><br>
 				<a href="signup.php">Dont have an account</a>
 			</form>
