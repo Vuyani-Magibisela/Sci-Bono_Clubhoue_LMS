c474297d0e6bc8914d54cfae8dd9f179fee9e387
vuyani-magibisela <vuyani.magibisela@live.com>
2025-06-23 11:28:16 +0200

    wls commit

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
 // Database credentials
 $host = "localhost";
-$user = "root";
-$password = "";
+$user = "vuksDev";
+$password = "Vu13#k*s3D3V";
 $dbname = "accounts";
 
 // Connect to MySQL database
